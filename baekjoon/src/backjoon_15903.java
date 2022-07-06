4648fc1a7cef6ca672c68293fe704117670be02b
qkrtjddms27 <qkrtjddms27@naver.com>
2022-07-06 21:41:49 +0900

    backjoon : 우선순위큐

--- baekjoon/src/backjoon_15903.java
+++ baekjoon/src/backjoon_15903.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class backjoon_15903 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class backjoon_15903 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i<N; i++)
+            queue.offer(Long.parseLong(st.nextToken()));
+
+        for(int i = 0; i<M; i++) {
+            long temp = queue.poll() + queue.poll();1
+            queue.offer(temp);
+            queue.offer(temp);
+        }
+
+        long answer = 0;
+
+        while(!queue.isEmpty()) {
+            answer += queue.poll();
+        }
+
+        System.out.println(answer);
+    }
+
 }
