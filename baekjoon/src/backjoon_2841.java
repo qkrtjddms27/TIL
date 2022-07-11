86c70a22a772dc4c1fd1bc0fc3875d5fcdf22c81
qkrtjddms27 <qkrtjddms27@naver.com>
2022-07-11 21:31:46 +0900

    bfs

--- baekjoon/src/backjoon_2841.java
+++ baekjoon/src/backjoon_2841.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class backjoon_2841 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class backjoon_2841 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        Stack<Integer>[] stacks = new Stack[N];
+        for(int i = 0; i<N; i++){
+            stacks[i] = new Stack<Integer>();
+        }
+        same : for(int i = 0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int f = Integer.parseInt(st.nextToken());
+
+            while(!stacks[m].isEmpty() && stacks[m].peek() >= f) {
+                if(stacks[m].peek() == f) continue same;
+                stacks[m].pop();
+                answer++;
+            }
+
+            stacks[m].push(f);
+            answer++;
+        }
+        System.out.println(answer);
+    }
 }
